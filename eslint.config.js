ae8234e5bf243fae31e4241bec1fc475d9ef5cf7
Nasir <nasirhossain1070@gmail.com>
2025-08-13 18:35:58 +0530

    Add dependencies and eslint configurations

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -24,6 +25,15 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+    {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
 ])
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import { Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      lms
-    </>
+    <Routes>
+      
+    </Routes>
   )
 }
 
