057a0cdfb383df24784f2b4d0f1e09a2af2c61e2
Nasir <nasirhossain1070@gmail.com>
2025-08-14 01:31:50 +0530

    Added basic layout and homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Routes } from 'react-router-dom'
 import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
+
+
 
 function App() {
 
   return (
-    <Routes>
-      
-    </Routes>
+    <>
+      <Routes>
+            <Route path='/' element={<HomePage/>}/>
+      </Routes>
+    </>
   )
 }
 
